3cf31d1768ed8c06ca8ec24696c77fdc52d643de
Dolphin Oracle <dolphinoracle@gmail.com>
2018-04-15 15:22:16 -0400

    revised autoinstall routine for nvme and mmcblk, and 32 bit UEFI

--- minstall.cpp
+++ minstall.cpp
@@ -366,6 +366,10 @@ bool MInstall::makeSwapPartition(QString dev)
 // create ESP at the begining of the drive
 bool MInstall::makeEsp(QString drv, int size)
 {
+    QString mmcnvmepartdesignator;
+    if (drv.contains("nvme") || drv.contains("mmcblk" )) {
+        mmcnvmepartdesignator = "p";
+    }
     int err = runCmd("parted -s " + drv + " mkpart primary 0 " + QString::number(size) + "MiB");
     if (err != 0) {
         qDebug() << "Could not create ESP";
@@ -374,7 +378,7 @@ bool MInstall::makeEsp(QString drv, int size)
 
     runCmd("parted -s " + drv + " set 1 esp on");   // sets boot flag and esp flag
 
-    err = runCmd("mkfs.msdos -F 32 " + drv + "1");
+    err = runCmd("mkfs.msdos -F 32 " + drv + mmcnvmepartdesignator + "1");
     if (err != 0) {
         qDebug() << "Could not format ESP";
         return false;
@@ -479,6 +483,8 @@ bool MInstall::makeDefaultPartitions()
     int prog = 0;
     bool uefi = isUefi();
     bool arch64 = is64bit();
+    QString mmcnvmepartdesignator;
+    mmcnvmepartdesignator.clear();
 
     QString rootdev, swapdev;
 
@@ -490,6 +496,10 @@ bool MInstall::makeDefaultPartitions()
         return false;
     }
 
+    if (drv.contains("nvme") || drv.contains("mmcblk" )) {
+        mmcnvmepartdesignator = "p";
+    }
+
     // entire disk, create partitions
     updateStatus(tr("Creating required partitions"), ++prog);
 
@@ -498,7 +508,7 @@ bool MInstall::makeDefaultPartitions()
     system("/sbin/swapoff -a 2>&1");
 
     // unmount root part
-    rootdev = drv + "1";
+    rootdev = drv + mmcnvmepartdesignator + "1";
     QString cmd = QString("/bin/umount -l %1 >/dev/null 2>&1").arg(rootdev);
     if (system(cmd.toUtf8()) != 0) {
         qDebug() << "could not umount: " << rootdev;
@@ -528,7 +538,7 @@ bool MInstall::makeDefaultPartitions()
 
     // allocate space for ESP
     int esp_size = 0;
-    if(uefi && arch64) { // if booted from UEFI and 64bit
+    if(uefi) { // if booted from UEFI and 64bit
         esp_size = 256;
         remaining -= esp_size;
     }
@@ -557,15 +567,15 @@ bool MInstall::makeDefaultPartitions()
         free = 0;
     }
 
-    if(uefi && arch64) { // if booted from UEFI and 64bit make ESP
+    if(uefi) { // if booted from UEFI make ESP
         // new GPT partition table
         int err = runCmd("parted -s " + drv + " mklabel gpt");
         if (err != 0 ) {
             qDebug() << "Could not create gpt partition table on " + drv;
             return false;
         }
-        rootdev = drv + "2";
-        swapdev = drv + "3";
+        rootdev = drv + mmcnvmepartdesignator + "2";
+        swapdev = drv + mmcnvmepartdesignator + "3";
         updateStatus(tr("Formating EFI System Partition (ESP)"), ++prog);
         if(!makeEsp(drv, esp_size)) {
             return false;
@@ -579,8 +589,8 @@ bool MInstall::makeDefaultPartitions()
             qDebug() << "Could not create msdos partition table on " + drv;
             return false;
         }
-        rootdev = drv + "1";
-        swapdev = drv + "2";
+        rootdev = drv + mmcnvmepartdesignator + "1";
+        swapdev = drv + mmcnvmepartdesignator + "2";
     }
 
     // create root partition
@@ -618,10 +628,13 @@ bool MInstall::makeDefaultPartitions()
         return false;
     }
 
-    if(uefi && arch64) { // set appropriate flags
-        runCmd("parted -s " + drv + " disk_set pmbr_boot on");
-    } else {
-        runCmd("parted -s " + drv + " set 1 boot on");
+    //if uefi is not detected, set flags based on GPT. Else don't set a flag...done by makeESP.
+    if(!uefi) { // set appropriate flags
+        if (isGpt(drv)) {
+            runCmd("parted -s " + drv + " disk_set pmbr_boot on");
+        } else {
+            runCmd("parted -s " + drv + " set 1 boot on");
+        }
     }
 
     system("sleep 1");
@@ -1081,8 +1094,14 @@ bool MInstall::isGpt(QString drv)
 
 bool MInstall::isUefi()
 {
-    return (system("test -d /sys/firmware/efi") == 0);
+    // return false if not uefi, or if a bad combination, like 32 bit iso and 64 bit uefi)
+    if (system("uname -m | grep -q i686") == 0 && system("grep -q 64 /sys/firmware/efi/fw_platform_size") == 0) {
+        return false;
+    } else {
+        return (system("test -d /sys/firmware/efi") == 0);
+    }
 }
+
 
 /////////////////////////////////////////////////////////////////////////
 // create the user, can not be rerun
